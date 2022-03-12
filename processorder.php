13da6d544417d7d9d7cf1da8da04c64e18557cd1
Rumburaq2 <93189782+Rumburaq2@users.noreply.github.com>
2022-03-12 10:57:18 +0100

    Update processorder.php

--- processorder.php
+++ processorder.php
@@ -1,4 +1,4 @@
-<!-- aka hiworld.php - uplne prvni zpracovaní formuláře s filtrem htmlchars() -->
+<!-- aka hiworld.php - trochu lepsi zpracovani vylepseneho formulare -->
 
 <DOCTYPE html>
 <html>
@@ -14,15 +14,54 @@
      $tireqty = $_POST['tireqty'];
      $oilqty = $_POST['oilqty'];
      $sparkqty = $_POST['sparkqty'];
+     $find = $_POST['find'];
 
-      date_default_timezone_set("UTC");
-      echo "<p>objednávka byla zprcována v ";
-      echo date("l jS \of F Y h:i:s A");
-      echo "</p>";
-      echo "<p>objednali jste si: </p>";
-      echo 'pneumatik: '.htmlspecialchars($tireqty).'<br />';
-      echo 'lahví oleje: '.htmlspecialchars($oilqty).'<br />';
-      echo 'zapalovacích svíček: '.htmlspecialchars($sparkqty).'<br />';
-    ?>
+     $totalqty = 0;
+     $totalqty = $tireqty + $oilqty + $sparkqty;
+     if($totalqty == 0){
+       echo "Na předchozí stránce jste si nic neobjednali ._. <br />";
+       exit;
+     }
+
+     date_default_timezone_set("UTC");
+     echo "<p>objednávka byla zprcována v ";
+     echo date("l jS \of F Y h:i:s A");
+     echo "</p>";
+     echo "<p>objednali jste si: </p>";
+     echo 'pneumatik: '.htmlspecialchars($tireqty).'<br />';
+     echo 'lahví oleje: '.htmlspecialchars($oilqty).'<br />';
+     echo 'zapalovacích svíček: '.htmlspecialchars($sparkqty).'<br />';
+
+     echo "<p>Objednáno položek: ".$totalqty."<br />";
+     $totalamount = 0.00;
+     define('TIREPRICE', 2500);
+     define('OILPRICE', 250);
+     define('SPARKPRICE', 100);
+     $totalamount = $tireqty * TIREPRICE +
+                   $oilqty * OILPRICE +
+                   $sparkqty * SPARKPRICE;
+     echo "Cena: ".number_format($totalamount, 2)." Kč<br />";
+     $taxrate = 0.20;  // výše DPH je 20%
+     $totalamount = $totalamount * (1 + $taxrate);
+     echo "Celková cena s DPH: ".number_format($totalamount,2)." Kč</p>";
+
+
+     if ($find == "a") {
+       echo "<p>Stálý zákazník.</p>";
+     }
+     elseif ($find == "b") {
+       echo "<p>Zákazník oslovený televizní reklamou.</p>";
+     }
+     elseif ($find == "c") {
+       echo "<p>Zákazník, který našel odkaz v telefonním seznamu.</p>";
+     }
+     elseif ($find == "d") {
+       echo "<p>Zákazník, kterého odkázal známý.</p>";
+     }
+     else {
+      echo "<p>Není jasné, jak nás tento zákazník našel.</p>";
+    }
+
+   ?>
   </body>
 </html>
